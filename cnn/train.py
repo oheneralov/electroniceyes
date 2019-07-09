e7a16adfd8203831bb2a974a8864f1560225d089
OHeneralov <alex@sykiq.com>
2019-07-09 12:13:57 +0300

    refactoring

--- cnn/train.py
+++ cnn/train.py
@@ -1,24 +1,25 @@
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D
 
 
-# dimensions of our images.
-img_width, img_height = 150, 150
+# dimensions of images.
+imageWidth, imageHeight = 150, 150
 
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_samples = 2000 # number of train images
-nb_validation_samples = 10 # number of validation samples
+trainSamplesNum = 2000 # number of train images
+validationSamplesNum = 10 # number of validation samples
+batchSize = 5
 epochs = 60
-batch_size = 5
+
 
 if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height)
+    input_shape = (3, imageWidth, imageHeight)
 else:
-    input_shape = (img_width, img_height, 3)
+    input_shape = (imageWidth, imageHeight, 3)
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), input_shape=input_shape))
@@ -44,34 +45,34 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-# this is the augmentation configuration we will use for training
+# augmentation config used for training
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True)
 
-# this is the augmentation configuration we will use for testing:
-# only rescaling
+# augmentation config used for testing:
+# just rescaling
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
+    target_size=(imageWidth, imageHeight),
+    batchSize=batchSize,
     class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
+    target_size=(imageWidth, imageHeight),
+    batchSize=batchSize,
     class_mode='binary')
 
 model.fit_generator(
     train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
+    steps_per_epoch=trainSamplesNum // batchSize,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size)
+    validation_steps=validationSamplesNum // batchSize)
 
-model.save_weights('first_try.h5')
+model.save_weights('cnnweights.h5')
